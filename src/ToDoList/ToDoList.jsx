7c458b5b727a71201b79eee8ef09b3457ca690ed
Harsha Buddhika <59781888+HarshaBuddhikaOfficial@users.noreply.github.com>
2025-04-07 23:06:20 +0530

    variables naming correctly

--- src/ToDoList/ToDoList.jsx
+++ src/ToDoList/ToDoList.jsx
@@ -2,36 +2,36 @@ import react, {useState} from 'react';
 import './ToDoList.css'
 
 function ToDoList(){
-    const [events, setEvents] = useState([]);
+    const [tasks, setTasks] = useState([]);
     
-    function addEventsToList(){
-        const adding = document.getElementById("addEvents").value;
-        document.getElementById("addEvents").value = "";
+    function addTasksToList(){
+        const adding = document.getElementById("addTasks").value;
+        document.getElementById("addTasks").value = "";
         if(adding == ""){
             alert("Text is empty");
         }
         else{
-            setEvents(a => [...a, adding]);
+            setTasks(a => [...a, adding]);
         }
     }
-    function removeEvents(indexToRemove){
-        setEvents(events.filter((_, i) => i !== indexToRemove));
+    function removeTasks(indexToRemove){
+        setTasks(tasks.filter((_, i) => i !== indexToRemove));
     }
-    function editEvents(){
-        
+    function editTasks(){
+        setTasks();
     }
 
     return(
         <div>
             <h2 className="hedding">To Do List</h2>
             <ul className="list">
-                {events.map((event, index) => <li key={index} >{event}
-                            <button onClick={() => removeEvents(index)} > Remove </button> 
-                            <button onClick={() => editEvents (index)}> Edit </button>
+                {tasks.map((event, index) => <li key={index} >{event}
+                            <button onClick={() => removeTasks(index)} > Remove </button> 
+                            <button onClick={() => editTasks (index)}> Edit </button>
                             </li>)}
             </ul>
-            <input type="text" id="addEvents" placeholder="Add Your Events" className="textAdd"/>
-            <button className="button" onClick={addEventsToList}> Add </button>
+            <input type="text" id="addTasks" placeholder="Add Your Events" className="textAdd"/>
+            <button className="button" onClick={addTasksToList}> Add </button>
         </div>
     );
 }
