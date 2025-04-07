db038474b2ce147a515f7f76a8b85429d2e4b127
Harsha Buddhika <59781888+HarshaBuddhikaOfficial@users.noreply.github.com>
2025-04-07 17:21:15 +0530

    styling done

--- src/ToDoList/ToDoList.jsx
+++ src/ToDoList/ToDoList.jsx
@@ -7,8 +7,12 @@ function ToDoList(){
     function addEventsToList(){
         const adding = document.getElementById("addEvents").value;
         document.getElementById("addEvents").value = "";
-
-        setEvents(a => [...a, adding]);
+        if(adding == ""){
+            alert("Text is empty");
+        }
+        else{
+            setEvents(a => [...a, adding]);
+        }
     }
     function removeEventsToList(indexToRemove){
         setEvents(events.filter((_, index) => index !== indexToRemove));
@@ -17,10 +21,9 @@ function ToDoList(){
     return(
         <div>
             <h2 className="hedding">To Do List</h2>
-            <ul>
-                {events.map((event, index) => <li key={index}>{event}
-                            <button onClick={() => removeEventsToList(index)} 
-                            > Remove </button> 
+            <ul className="list">
+                {events.map((event, index) => <li key={index} >{event}
+                            <button onClick={() => removeEventsToList(index)} > Remove </button> 
                             <button> Edit </button>
                             </li>)}
             </ul>
