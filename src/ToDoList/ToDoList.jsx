7e0523435fdaa5b2d79a3a18e4e98709618246a2
Harsha Buddhika <59781888+HarshaBuddhikaOfficial@users.noreply.github.com>
2025-04-17 21:23:39 +0530

    now fully compled the project

--- src/ToDoList/ToDoList.jsx
+++ src/ToDoList/ToDoList.jsx
@@ -5,13 +5,13 @@ function ToDoList(){
     const [tasks, setTasks] = useState([]);
     
     function addTasks(){
-        const adding = document.getElementById("addTasks").value;
-        document.getElementById("addTasks").value = "";
+        const adding = document.getElementById("addTasks").value; //taking enterd text and storing in varible
+        document.getElementById("addTasks").value = "";  //erase task enterd bar
         if(adding == ""){
             alert("Text is empty");
         }
         else{
-            setTasks(a => [...a, {text: adding, done:false}]);
+            setTasks(a => [...a, {text: adding, done:false}]); 
         }
     }
     function removeTasks(indexToRemove){
