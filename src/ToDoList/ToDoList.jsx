d51725cac0a842c9d192d3adc63ee5b98bb09f91
Harsha Buddhika <59781888+HarshaBuddhikaOfficial@users.noreply.github.com>
2025-04-17 18:22:37 +0530

    done function implimented
    mark as done function implemented and now its works well

--- src/ToDoList/ToDoList.jsx
+++ src/ToDoList/ToDoList.jsx
@@ -1,4 +1,4 @@
-import react, {useState} from 'react';
+import React, { useState } from 'react';
 import './ToDoList.css'
 
 function ToDoList(){
@@ -11,29 +11,45 @@ function ToDoList(){
             alert("Text is empty");
         }
         else{
-            setTasks(a => [...a, adding]);
+            setTasks(a => [...a, {text: adding, done:false}]);
         }
     }
     function removeTasks(indexToRemove){
         setTasks(tasks.filter((_, index) => index !== indexToRemove)); //_ is not used parameter(elements)
     }
     function editTasks(index){
-        let newTask = prompt('Edit the task Here');
+        let newTask = prompt('Edit the task Here', tasks[index].text);
+        if (newTask !== null && newTask.trim() !== "") {
         const updatedTasks = [...tasks];
-        updatedTasks[index] = newTask;
+        updatedTasks[index].text = newTask;
+        setTasks(updatedTasks);
+        }
+    }
+    function handleDone(index){
+        const updatedTasks = [...tasks];
+        updatedTasks[index].done = !updatedTasks[index].done;
         setTasks(updatedTasks);
     }
 
     return(
         <div>
             <h2 className="hedding">To Do List</h2>
-            <input type="text" id="addTasks" placeholder="Enter a Task..." className="textAdd"/>
+            <input  type="text"
+                    id="addTasks"
+                    placeholder="Enter a Task..."
+                    className="textAdd"/>
             <button className="button" onClick={addTasks}> Add </button>
             <ul className="list">
-                {tasks.map((event, index) => <li key={index} >{event}
+                {tasks.map((task, index) => (
+                            <li key={index} className="list-item">
+                                <span
+                                    className={`list-item ${task.done ? "done" : ""}`}
+                                    onClick={() => handleDone(index)}
+                                    > {task.text}
+                                </span>
                             <button onClick={() => removeTasks(index)}> Remove </button>
                             <button onClick={() => editTasks (index)}> Edit </button>
-                            </li>)}
+                            </li>))}
             </ul> 
         </div>
     );
